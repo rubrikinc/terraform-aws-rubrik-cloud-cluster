241808db86daa841c990b11ced0966586d43e4e5
Drew Russell <Drew.Russell@rubrik.com>
2019-03-15 10:07:53 -0500

    updated variables

--- main.tf
+++ main.tf
@@ -1,3 +1,10 @@
+terraform {
+  required_version = ">= 0.10.3" # introduction of Local Values configuration language feature
+}
+
+#############################
+# Dynamic Variable Creation #
+#############################
 resource "null_resource" "create_cluster_node_name" {
   count = "${var.number_of_nodes}"
 
@@ -23,6 +30,14 @@ data "aws_ami" "rubrik_cloud_cluster" {
   }
 }
 
+data "aws_subnet" "default_gateway" {
+  id = "${var.aws_subnet_id}"
+}
+
+###############################
+# Create EC2 Instances in AWS #
+###############################
+
 resource "aws_instance" "rubrik_cluster" {
   count                  = "${var.number_of_nodes}"
   instance_type          = "${var.aws_instance_type}"
@@ -34,7 +49,7 @@ resource "aws_instance" "rubrik_cluster" {
     Name = "${element(local.cluster_node_name, count.index)}"
   }
 
-  disable_api_termination = false
+  disable_api_termination = "${var.aws_disable_api_termination}"
 
   ebs_block_device {
     device_name = "/dev/sdb"
@@ -58,6 +73,10 @@ resource "aws_instance" "rubrik_cluster" {
   }
 }
 
+######################################
+# Bootstrap the Rubrik Cloud Cluster #
+######################################
+
 provider "rubrik" {
   node_ip  = "${aws_instance.rubrik_cluster.0.private_ip}"
   username = ""
@@ -68,12 +87,12 @@ resource "rubrik_bootstrap" "bootstrap_rubrik" {
   cluster_name           = "${var.cluster_name}"
   admin_email            = "${var.admin_email}"
   admin_password         = "${var.admin_password}"
-  management_gateway     = "${var.aws_subnet_gateway}"
-  management_subnet_mask = "${var.aws_subnet_mask}"
+  management_gateway     = "${cidrhost(data.aws_subnet.default_gateway.cidr_block, 1)}"
+  management_subnet_mask = "${cidrnetmask(data.aws_subnet.default_gateway.cidr_block)}"
   dns_search_domain      = "${var.dns_search_domain}"
   dns_name_servers       = "${var.dns_name_servers}"
   ntp_servers            = "${var.ntp_servers}"
-  enable_encryption      = "${var.enable_encryption}"
+  enable_encryption      = false
 
   node_config = "${zipmap(local.cluster_node_name, local.cluster_node_ips)}"
 }
--- variables.tf
+++ variables.tf
@@ -1,62 +1,55 @@
 variable "aws_instance_type" {
-  description = ""
+  description = "The type of instance to use as the Cloud Cluster nodes."
   default     = "m5.xlarge"
 }
 
+variable "aws_disable_api_termination" {
+  description = "If true, enables EC2 Instance Termination Protection"
+  default     = true
+}
+
 variable "aws_vpc_security_group_ids" {
-  description = ""
+  description = "A list of security group IDs to associate with the Cloud Cluster."
 }
 
 variable "aws_subnet_id" {
-  description = ""
-}
-
-variable "aws_subnet_gateway" {
-  description = ""
-}
-
-variable "aws_subnet_mask" {
-  description = ""
+  description = "The VPC Subnet ID to launch the Cloud Cluster in."
 }
 
 ####
 variable "number_of_nodes" {
-  description = ""
+  description = "The total number of nodes in the Cloud Cluster"
   default     = 4
 }
 
 variable "cluster_disk_size" {
-  description = ""
+  description = "The size of each the three data disks in each node."
   default     = "1024"
 }
 
 variable "cluster_name" {
-  description = ""
+  description = "Unique name to assign to the Rubrik cluster. This will also be used to populate the EC2 instance name tag. For example, rubrik-cloud-cluster-1, rubrik-cloud-cluster-2 etc."
   default     = "rubrik-cloud-cluster"
 }
 
 variable "admin_email" {
-  description = ""
+  description = "The Rubrik cluster sends messages for the admin account to this email address."
 }
 
 variable admin_password {
-  description = ""
+  description = "Password for the Cloud Cluster admin account."
   default     = "RubrikGoForward"
 }
 
 variable "dns_search_domain" {
-  description = ""
+  description = "List of search domains that the DNS Service will use to resolve hostnames that are not fully qualified."
 }
 
 variable "dns_name_servers" {
-  description = ""
+  description = "List of the IPv4 addresses of the DNS servers."
 }
 
 variable "ntp_servers" {
-  description = ""
+  description = "List of FQDN or IPv4 addresses of a network time protocol (NTP) server(s)"
   default     = ["8.8.8.8"]
-}
-
-variable "enable_encryption" {
-  description = ""
 }
